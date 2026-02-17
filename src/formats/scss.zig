9ba1f84e24bb35bd31abb8d6364fbb3680ecc26e
Valentyn Yakymenko <vyakimenko@codeminders.com>
2026-02-17 18:04:19 +0200

    Simplify @media hoisting - only check when encountering @
    - Only check for @media when we encounter @ character
    - Removes complex whitespace handling that was causing issues
    - Fixes character skipping problems

--- src/formats/scss.zig
+++ src/formats/scss.zig
@@ -806,13 +806,9 @@ pub const Parser = struct {
                 try result.append(self.allocator, ch);
                 i += 1;
                 continue;
-            } else if (in_rule) {
+            } else if (in_rule and ch == '@') {
                 var check_pos = i;
-                skipWhitespaceInSlice(input, &check_pos);
                 if (check_pos + 5 < input.len and std.mem.eql(u8, input[check_pos..check_pos+6], "@media")) {
-                    if (check_pos > i) {
-                        try result.appendSlice(self.allocator, input[i..check_pos]);
-                    }
                     const media_start = check_pos;
                     i = check_pos + 6;
                     skipWhitespaceInSlice(input, &i);
@@ -857,12 +853,7 @@ pub const Parser = struct {
                         continue;
                     } else {
                         i = check_pos;
-                        continue;
                     }
-                } else if (check_pos > i) {
-                    try result.appendSlice(self.allocator, input[i..check_pos]);
-                    i = check_pos;
-                    continue;
                 }
             }
             
